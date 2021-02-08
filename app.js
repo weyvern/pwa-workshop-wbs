d1944f4d4454c68c83f384c1d5949a7ff7784fcd
Jorge Paul <jserranopaul@gmail.com>
2021-02-08 12:53:16 +0100

    starter app

---
+++ app.js
@@ -0,0 +1,39 @@
+const api = 'https://jsonplaceholder.typicode.com/posts';
+const postsContainer = document.getElementById('posts');
+postsContainer.innerHTML = `
+    <div class='col'>
+        <div class="row justify-content-center align-items-center">
+            <img src='/assets/200.gif' alt='loading'>
+        </div>
+    </div>
+`;
+const getPosts = async () => {
+  try {
+    const res = await fetch(api);
+    const posts = await res.json();
+    postsContainer.innerHTML = '';
+    posts.forEach(post => {
+      let postItem = document.createElement('div');
+      postItem.classList = 'col-md-4 mb-4';
+      postItem.innerHTML = `
+          <div class="card">
+              <img class="card-img-top" src="https://picsum.photos/200/200" alt="Card image cap">
+              <div class="card-body">
+                  <h5 class="card-title">${post.title}</h5>
+                  <p class="card-text">${post.body}</p>
+              </div>
+          </div>
+      `;
+      postsContainer.appendChild(postItem);
+    });
+  } catch (error) {
+    postsContainer.innerHTML = `
+        <div class='col'>
+            <div class="row justify-content-center align-items-center">
+                <h2>Apparently, something went wrong</h2>
+            </div>
+        </div>
+      `;
+  }
+};
+getPosts();
