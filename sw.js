c99d152b5fb4af94a0bb3c7aab136608fc64d1cc
Jorge Paul <jserranopaul@gmail.com>
2021-02-08 18:16:34 +0100

    caching bootstrap and fonts

--- sw.js
+++ sw.js
@@ -1,7 +1,15 @@
 // Caching
 const cacheName = 'random-posts-cache';
 // Resources we want to cache
-const resourcesToPreCache = ['/', '/index.html', 'app.js'];
+const resourcesToPreCache = [
+  '/',
+  '/index.html',
+  'app.js',
+  '/assets/200.gif',
+  'https://bootswatch.com/4/flatly/bootstrap.min.css',
+  'https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,400;0,700;1,400&display=swap',
+  'https://fonts.gstatic.com/s/lato/v17/S6uyw4BMUTPHjx4wXg.woff2'
+];
 // Install event
 self.addEventListener('install', async event => {
   try {
