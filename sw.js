f7c499fa42a803b8a9947c1a4a43f6deac84bf43
Jorge Paul <jserranopaul@gmail.com>
2021-02-08 18:07:13 +0100

    caching + working when offline

--- sw.js
+++ sw.js
@@ -1,12 +1,23 @@
+// Caching
+const cacheName = 'random-posts-cache';
+// Resources we want to cache
+const resourcesToPreCache = ['/', '/index.html', 'app.js'];
 // Install event
-self.addEventListener('install', event => {
-  console.log('Install event!');
+self.addEventListener('install', async event => {
+  try {
+    const cache = await caches.open(cacheName);
+    cache.addAll(resourcesToPreCache);
+    console.log('Success, resources were cached!');
+  } catch (error) {
+    console.log('Error while caching: ', error);
+  }
 });
 // Activation event
 self.addEventListener('activate', event => {
   console.log('Activate event!');
 });
 // Fetch event
-self.addEventListener('fetch', event => {
-  console.log('Fetch intercepted for: ', event.request.url);
+self.addEventListener('fetch', async event => {
+  const cachedResponse = await event.respondWith(caches.match(event.request));
+  return cachedResponse || fetch(event.request);
 });
